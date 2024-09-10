ae5173a5023fb0ada44a42266a1e60c2c0351a68
TDangerW <thiranweerasinghe@outlook.com>
2024-09-10 18:12:54 +0530

    Project cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header></header>
+      <body>
+        Collections Men Women About Contact Sneaker Company Fall Limited Edition
+        Sneakers These low-profile sneakers are your perfect casual wear
+        companion. Featuring a durable rubber outer sole, they’ll withstand
+        everything the weather can offer. $125.00 50% $250.00 0 Add to cart
+      </body>
     </div>
   );
 }
